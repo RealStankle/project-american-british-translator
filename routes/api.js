e30ce69b2aa837b003fbb332efc8edb85c9e3623
Furkan Emin <femincanx@gmail.com>
2023-01-28 13:17:22 +0300

    feat: add /translate route

--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,37 @@
 const Translator = require('../components/translator.js');
 
 module.exports = function (app) {
-  
   const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
+  app.route('/api/translate').post((req, res) => {
+    const { text: inputText, locale } = req.body;
+
+    if (inputText === undefined || locale === undefined) {
+      return res.json({ error: 'Required field(s) missing' });
+    }
+
+    const textValidation = translator.validateText(inputText);
+    const localeValidation = translator.validateLocale(locale);
+
+    if (!textValidation.result || !localeValidation.result) {
+      return res.json({
+        error: textValidation.errorMessage || localeValidation.errorMessage,
+      });
+    }
+
+    const { text, translation, translationWithHighlight } =
+      translator.translate(locale, inputText);
+
+    if (text === translation) {
+      return res.json({
+        text,
+        translation: 'Everything looks good to me!',
+      });
+    }
+
+    res.json({
+      text,
+      translation: translationWithHighlight,
     });
+  });
 };
